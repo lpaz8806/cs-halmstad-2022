3f4babd6fef3e7b225e178eca21f88fa30727c1a
Luis Paz <lpaz8806@gmail.com>
2022-11-10 08:56:19 +0100

    io

--- L03_IO/Program.cs
+++ L03_IO/Program.cs
@@ -1,3 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿var pathToFile = "hello_io.txt";
+var pathToDir = "/Users/lpaz/Projects/";
 
-Console.WriteLine("Hello, World!");
+
+#if false
+Console.Clear();
+PrintFiles(pathToDir);
+
+
+void PrintFiles(string rootDir)
+{
+    if (!Directory.Exists(rootDir))
+        return;
+    
+    foreach (var file in Directory.EnumerateFiles(rootDir))
+    {
+        Console.WriteLine(file);
+    }
+    foreach (var dir in Directory.EnumerateDirectories(rootDir))
+    {
+        PrintFiles(dir);
+    }
+}
+#endif
+
+#if false
+using var reader = new StreamReader(pathToFile);
+while (!reader.EndOfStream)
+{
+    Console.WriteLine(reader.ReadLine());
+}
+#endif
+
+
+#if false
+using var writer = new StreamWriter(pathToFile);
+while (true)
+{
+    var line = Console.ReadLine() ?? string.Empty;
+    if(string.IsNullOrEmpty(line))
+        break;
+    
+    writer.WriteLine(line);
+}
+#endif
+
+#if false
+Console.Write("Tell me something in many lines: ");
+
+List<string> sentences = new();
+while (true)
+{
+    var sentence = Console.ReadLine();
+    if(string.IsNullOrEmpty(sentence))
+        break;
+    sentences.Add(sentence);
+}
+
+File.AppendAllLines(pathToFile, sentences);
+
+// The whole file
+Console.WriteLine();
+Console.WriteLine("The whole file contains now:");
+var content = File.ReadAllText(pathToFile);
+Console.WriteLine(content);
+#endif
---
+++ L03_IO/StringBuilder.cs
@@ -0,0 +1,8 @@
+namespace L03_IO;
+using DotNetStringBuilder = System.Text.StringBuilder;
+
+public class StringBuilder
+{
+    private DotNetStringBuilder x;
+    
+}
