d1284f663fa78d97c0335de2a7af234a5eee49a0
Luis Paz <lpaz8806@gmail.com>
2022-11-17 11:29:35 +0100

    ex 1

--- L08_Events/Program.cs
+++ L08_Events/Program.cs
@@ -5,10 +5,15 @@ Agenda:
     [X] Anonymous delegates / Lambda expressions
     [] Events
 */
+
+using Events.Examples;
+using Events.Exercises;
+
 class Program
 {
     static void Main(string[] args)
     {
-        
+        ReduceArray.Run();
     }
+    
 }
